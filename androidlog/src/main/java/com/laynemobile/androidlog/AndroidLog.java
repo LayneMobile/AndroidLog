f574e86dd301152b21f271718542471cd9acbca4
Layne Penney <layne@laynemobile.com>
2016-01-16 22:41:21 -0600

    update package

--- androidlog/src/main/java/com/laynemobile/androidlog/AndroidLog.java
+++ androidlog/src/main/java/com/laynemobile/androidlog/AndroidLog.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.laynepenney.util;
+package com.laynemobile.androidlog;
 
 import java.util.Locale;
 
--- androidlog/src/main/java/com/laynemobile/androidlog/Logger.java
+++ androidlog/src/main/java/com/laynemobile/androidlog/Logger.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.laynepenney.util;
+package com.laynemobile.androidlog;
 
 import android.util.Log;
 
